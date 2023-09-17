2d4f5e38264d208dc110511e67de43a64d2b12ce
Tommer <Tommer_t@hotmail.com>
2023-09-17 13:17:32 +0300

    Created the project

---
+++ src/main/java/SampleScript.java
@@ -0,0 +1,36 @@
+import org.powbot.api.Condition;
+import org.powbot.api.rt4.*;
+import org.powbot.api.script.*;
+
+// Example of GUI option which asks for a string
+@ScriptConfiguration(name = "NPC", description = "NPC to fight", optionType = OptionType.STRING)
+@ScriptConfiguration(name = "Example GUI option", description = "Example GUI option", optionType = OptionType.STRING, visible = false)
+@ScriptManifest(name = "Example Script", description = "Example Script", version = "1")
+
+
+public class SampleScript extends AbstractScript {
+
+    private String GuiNPC; // Creates an empty
+    @Override
+    public void onStart() {
+        // Logic to happen before poll() starts
+        boolean GuiNPC = getOption("NPC"); // Gets value of specific GUI key and saves it in a variable.
+    }
+
+    @Override
+    public void poll() {
+        // Looped tasks go here. Example below is of a simple fighter script
+
+    }
+
+
+    @ValueChanged(keyName = "NPC") // Listens for changes to the specific GUI config key.
+    private void exampleConfigChanged(Boolean valueChanged) {
+        updateVisibility("Example GUI option", true); // Changes visibility of the second GUI config key if another key is modified
+    }
+
+    public static void main(String[] args) {
+        // Start your script with this function. Make sure your device is connected via ADB, and only one is connected
+        new SampleScript().startScript();
+    }
+}
